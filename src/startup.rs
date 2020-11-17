379b61fef69b9514f7ad001f52e445642763a1e4
Fatih Karahan <hseyin97@gmail.com>
2020-11-17 16:53:13 +0300

    added telemetry

--- src/startup.rs
+++ src/startup.rs
@@ -1,21 +1,19 @@
+use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
-
-use crate::routes::{health_check, subscribe};
+use tracing_actix_web::TracingLogger;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
-    // Wrap the pool in an Arc smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
+            // Instead of `Logger::default`
+            .wrap(TracingLogger)
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
-            // Our pool is already wrapped in an Arc pointer:
-            // using .data would add another Arc pointer on top
-            // of the existing one - an unnecessary indirection.
-            // .app_data instead does not perform an additional layer of wrapping.
             .app_data(db_pool.clone())
     })
     .listen(listener)?
